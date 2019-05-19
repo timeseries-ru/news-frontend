8bab55d052eac42c73c86f850971cd69d1b5f8cc
Andrey <andrsaz84@gmail.com>
2019-05-19 08:04:07 +0700

    layout initial

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,100 +1,77 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      :mini-variant.sync="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          router
-          :to="item.to"
-          :key="i"
-          v-for="(item, i) in items"
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
+  <v-app>
+    <v-toolbar fixed app>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-text-field
+        placeholder="Поиск" append-icon="search" @keyup.enter="search" v-model="term"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn flat
+        nuxt to="/blog"
+      >
+        Блог
+      </v-btn>
+      <v-btn flat
+        nuxt to="/about"
+      >
+        О проекте
+      </v-btn>
       <v-btn
         icon
-        @click.stop="rightDrawer = !rightDrawer"
+        @click="toggleSidebar()"
       >
-        <v-icon>menu</v-icon>
+        <v-icon>settings</v-icon>
       </v-btn>
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
+      <v-footer>
+        <span>&copy; {{ new Date().getFullYear() }}</span>
+      </v-footer>
     </v-content>
     <v-navigation-drawer
       temporary
-      :right="right"
-      v-model="rightDrawer"
+      clipped
+      right
+      v-model="sidebar"
       fixed
+      width="450"
     >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
     </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
+  import { mapMutations } from 'vuex'
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        term: '',
+        title: 'News Insights'
       }
+    },
+    computed: {
+      sidebar: {
+        get () {
+          return this.$store.state.sidebar
+        },
+        set (value) {
+          this.setSidebar(value)
+        }
+      }
+    },
+    methods: {
+      search () {
+        this.$router.push('/search/' + this.term)
+        this.term = ''
+      },
+      ...mapMutations({
+        toggleSidebar: 'toggleSidebar',
+        setSidebar: 'setSidebar'
+      })
     }
   }
 </script>
--- store/index.js
+++ store/index.js
@@ -3,6 +3,9 @@ export const state = () => ({
 })
 
 export const mutations = {
+  setSidebar (state, value) {
+    state.sidebar = value
+  },
   toggleSidebar (state) {
     state.sidebar = !state.sidebar
   }
